c53d76c0d5e4fab74c3e6c30e1287ef43d60b4fa
jihan24sia <jihan24si@mahasiswa.pcr.ac.id>
2025-10-02 07:28:47 +0700

    melakukan perubahan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 
 
@@ -23,3 +24,4 @@ Route::get('/mahasiswa/{param1}', [MahasiswaController::class,'show']);
 Route::get('/about', function () {
     return view('halaman-about');
 });
+Route::get('/home',[HomeController::class,'index']);
