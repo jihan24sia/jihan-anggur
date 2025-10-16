a5f58687505a230040f463417d7ff4d67d61b17d
jihan24sia <jihan24si@mahasiswa.pcr.ac.id>
2025-10-16 08:00:09 +0700

    membuat pelanggan controller

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 
 Route::get('/', function () {
@@ -35,3 +36,5 @@ Route::post('question/store', [QuestionController::class, 'store'])
 
 Route::get('/dashboard',[DashboardController::class,'index'])
 ->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
